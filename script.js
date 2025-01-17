f17eee908209e5ca016c6f03a312e61fbf3611d9
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-17 15:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-//your JS code here. If required.
 const codeInputs = document.querySelectorAll('.code');
-codeInputs.forEach((input, index) =>{
-	input.addElementListener('input', (event) =>{
-		const value = event.target.value;
 
-		if(value && index < codeInputs.length-1){
-			codeInputs[index+1].focus();
-		}
-	});
-	input.addEventListener('keydown', (event)=>{
-		if(event.key === 'Backspace' && index > 0){
-			event.target.value ='';
-			codeInputs[index-1].focus();
-		}
-	});
+codeInputs.forEach((input, index) => {
+  input.addEventListener('input', (event) => {
+    const value = event.target.value;
+
+    // If input is not empty and next input exists
+    if (value && index < codeInputs.length - 1) {
+      // Focus on the next input field
+      codeInputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (event) => {
+    // If backspace is pressed and there is a previous input field
+    if (event.key === 'Backspace' && index > 0) {
+      // Clear the current input field
+      event.target.value = '';
+      // Focus on the previous input field
+      codeInputs[index - 1].focus();
+    }
+  });
 });
