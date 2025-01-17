57ca62be739e1f89f61e9cb3e95e97eec23a577e
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-17 14:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function moveFocus(currentInput, event){
+	if (event.key!== "Backspace" && currentInput.value!== ""){
+		const nextInput = currentInput.nextElementSibling;
+		if(nextInput){
+			nextInput.focus();
+		}
+	} 
+	if (event.key === "Backspace" && currentInput.value === "") {
+        const previousInput = currentInput.previousElementSibling;
+        if (previousInput) {
+            previousInput.focus();
+        }
+    }
+}
