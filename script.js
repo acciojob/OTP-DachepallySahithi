de61070265e5d076350a930189daa0e6c0589022
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-18 15:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-codeInputs.forEach((input, index) => {
-  input.addEventListener("keydown", (event) => {
-    if (event.key === "Backspace") {
-      if (index > 0) {
-        input.value = ""; // Clear the current input field
-        codeInputs[index - 1].focus(); // Focus on the previous input field
-      }
-    }
-  });
-});
+const codeInputs = document.querySelectorAll(".code");
 
-// Improved focus shifting
 codeInputs.forEach((input, index) => {
   input.addEventListener("input", () => {
     if (input.value !== "") {
       if (index < codeInputs.length - 1) {
-        codeInputs[index + 1].focus(); // Focus on the next input field
+        codeInputs[index + 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace") {
+      if (index > 0) {
+        codeInputs[index - 1].focus();
+        codeInputs[index - 1].value = "";
       }
     }
   });
