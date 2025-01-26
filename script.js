4415594107bfcd374f10831912b99d93d19a8576
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-26 20:07:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,16 +13,14 @@ const inputs = document.querySelectorAll('.code');
         }
       });
 
-      input.addEventListener('paste', (e) => {
-        e.preventDefault();
-        const pasteData = e.clipboardData.getData('text').slice(0, inputs.length);
-        pasteData.split('').forEach((char, i) => {
-          if (inputs[i]) {
-            inputs[i].value = char;
-          }
-        });
-        const nextInput = pasteData.length - 1;
-        if (inputs[nextInput]) {
+      input.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace' && !input.value && index > 0) {
+    inputs[index - 1].focus();
+    inputs[index - 1].setAttribute('id', `code-${index}`);
+  }
+});
+const nextInput = pasteData.length - 1;
+		if (inputs[nextInput]) {
           inputs[nextInput].focus();
         }
       });
