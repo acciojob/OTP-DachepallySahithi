04c643f8b5874110e212dd2f666c3866c2ae0018
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-28 10:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-const inputs = document.querySelectorAll('.code');
+const inputs = document.querySelectorAll(".code");
 
-    inputs.forEach((input, index) => {
-      input.addEventListener('input', (e) => {
-        if (e.target.value.length > 0 && index < inputs.length - 1) {
-          inputs[index + 1].focus();
-        }
-      });
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    if (value) {
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    }
+  });
 
-      input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && !input.value && index > 0) {
-          inputs[index - 1].focus();
-        }
-      });
-
-      input.addEventListener('keydown', (e) => {
-  if (e.key === 'Backspace' && !input.value && index > 0) {
-    inputs[index - 1].focus();
-    inputs[index - 1].setAttribute('id', `code-${index}`);
-  }
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (!e.target.value && index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
 });
-const nextInput = pasteData.length - 1;
-		if (inputs[nextInput]) {
-          inputs[nextInput].focus();
-        }
-      });
-    });
